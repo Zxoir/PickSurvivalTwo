e8cc3744d094ef3fb165174dbc8747eb2262121c
zxoir <zxoirg@gmail.com>
2024-06-24 11:54:29 -0800

    Removed Command Delay

--- src/main/java/me/zxoir/picksurvivaltwo/listeners/DelayListener.java
+++ src/main/java/me/zxoir/picksurvivaltwo/listeners/DelayListener.java
@@ -5,7 +5,6 @@ import me.zxoir.picksurvivaltwo.util.Colors;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
