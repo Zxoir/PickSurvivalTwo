42f8d4ddb040942e3da6d62cd04ea1b83d83584c
zxoir <zxoirg@gmail.com>
2024-06-28 02:17:03 -0800

    Removed unnecessary if statements

--- src/main/java/me/zxoir/picksurvivaltwo/listeners/BackListener.java
+++ src/main/java/me/zxoir/picksurvivaltwo/listeners/BackListener.java
@@ -1,11 +1,14 @@
 package me.zxoir.picksurvivaltwo.listeners;
 
+import me.zxoir.picksurvivaltwo.PickSurvivalTwo;
 import me.zxoir.picksurvivaltwo.data.PlayerProfile;
 import me.zxoir.picksurvivaltwo.managers.PlayerProfileManager;
 import me.zxoir.picksurvivaltwo.util.GlobalCache;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.jetbrains.annotations.NotNull;
 
@@ -30,14 +33,31 @@ public class BackListener implements Listener {
         if (!event.getTo().getWorld().equals(GlobalCache.getSpawnWarp().getLocation().getWorld()) || event.getFrom().getWorld().equals(GlobalCache.getSpawnWarp().getLocation().getWorld()))
             return;
 
-        if (event.getFrom().getWorld().getName().equalsIgnoreCase("witp"))
-            return;
-
-        if (event.getFrom().getWorld().getName().equalsIgnoreCase("witp") && event.getTo().getWorld().getName().equalsIgnoreCase("witp"))
-            return;
-
         if (!player.isOp())
             profile.getCache().setBackLocation(event.getFrom());
+    }
+
+    @EventHandler
+    public void onQuit(@NotNull PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        PlayerProfile profile = PlayerProfileManager.getPlayerProfile(player.getUniqueId());
+
+        if (profile.getCache().getBackLocation() != null) {
+            PickSurvivalTwo.getDataFile().getConfig().set("back." + player.getUniqueId(), profile.getCache().getBackLocation());
+            PickSurvivalTwo.getDataFile().saveConfig();
+        }
+    }
+
+    @EventHandler
+    public void onJoin(@NotNull PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        PlayerProfile profile = PlayerProfileManager.getPlayerProfile(player.getUniqueId());
+
+        if (PickSurvivalTwo.getDataFile().getConfig().contains("back." + player.getUniqueId())) {
+            profile.getCache().setBackLocation(PickSurvivalTwo.getDataFile().getConfig().getLocation("back." + player.getUniqueId()));
+            PickSurvivalTwo.getDataFile().getConfig().set("back." + player.getUniqueId(), null);
+            PickSurvivalTwo.getDataFile().saveConfig();
+        }
     }
 
 }
